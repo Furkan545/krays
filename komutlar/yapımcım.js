251d2d9d0376a2327a0377f0aeb09d7cdddd909a
Glitch (screeching-pram) <none>
2019-03-28 11:52:09 +0000

    🐝🌄 Checkpoint ./komutlar/yapımcım.js:2423501/124

--- komutlar/yapımcım.js
+++ komutlar/yapımcım.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
 var embed = new Discord.RichEmbed()
 
 .setTitle(`Özel Kodalama Bot Bilgi`)
-.addField(`Kodlayan`,`<@441169716547944448>`)
+.addField(`Kodlayan`,`<@530713851675672587>`)
 .setAuthor(`Bot Bilgi`,message.author.avatarURL,)
 .setColor("GREEN")
 .setThumbnail(client.user.avatarURL) // şş saşlkda guild
@@ -12,7 +12,6 @@ var embed = new Discord.RichEmbed()
 .addField(`Botun Olduğu Sunucu Sayısı`,client.guilds.size) // bu varmı böyle bişe hm unutum 1 dk
 .addField(`Botaki Komut Sayısı`,client.commands.size)
 //oldumu :D ?
-.addField(`Bot Sahibi`,client.ayarlar.official_sahip) //tamam anladım çekme en kolay iş
 .addField(`Sahip`, `<@441169716547944448>`,true) //imdatt efe dc bak bi haa burda niye hata var
 .addField(`Botun Geliştiricisi`,`<@530713851675672587>`,true)
 .addField(`Bot Discord.js Versiyon`,Discord.version)
