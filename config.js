7be2a53f5cf968e84495eff46bffedead5dab4f1
Glitch (osman8x) <none>
2019-04-29 21:17:57 +0000

    🌷🍯 Checkpoint ./config.js:3756202/184 ./index.js:3756202/50

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ const config = {
   "ownerID": "495679360271908864", //kendi IDınızı yazınız
   "admins": ["495679360271908864"],
   "support": ["495679360271908864"],
-  "token": "", //botunuzun tokenini yazınız
+  "token": "NTcyNTI5MTc5Mzg4NTQzMDI0.XMdn6g.TiZBUly0Wl0SFsqEs1EjGyCmw3o", //botunuzun tokenini yazınız
   "dashboard" : {
-    "oauthSecret": "mTLUb2kW8S2M74dHcJgRzEsYtc6tLzHk", //botunuzun secretini yazınız 
-    "callbackURL": "https://osman8x.glitch.me/callback",//site URLnizi yazınız /callback kısmını silmeyiniz!
+    "oauthSecret": "vjgjLGg4zZDrYJfQU_9HW44m2zLNI65N", //botunuzun secretini yazınız 
+    "callbackURL": "",//site URLnizi yazınız /callback kısmını silmeyiniz!
     "sessionSecret": "9999999999999999999999999", //kalsın
-    "domain": "https://turkestr.glitch.me/" //site URLnizi yazınız!
+    "domain": "" //site URLnizi yazınız!
   }
 };
 
