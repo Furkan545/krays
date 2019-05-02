6ab71055c90240b484de60aac3952e5bdd51f9a6
Glitch (osman8x) <none>
2019-05-02 16:54:11 +0000

    🍵🐝 Checkpoint ./komutlar/yardım1.js:3756202/9 ./komutlar/kayıt.js:3756202/2202

--- komutlar/kayıt.js
+++ komutlar/kayıt.js
@@ -1,23 +1,56 @@
 const Discord = require('discord.js');
+module.exports.run = async (client, msg, args) => {
 
-exports.run = function(client, message) {
-  var role = message.guild.roles.find(role => role.name === "✦─Kullanıcı─✦");
-  var rol = message.guild.roles.find(rol => rol.name === "✦─Kayıtsız Üye─✦");
-  message.member.removeRole(rol);
-  message.member.addRole(role);
-  message.channel.send(`**${message.author.username} kullanıcısının kaydı yaptırıldı.**`);
-  message.channel.bulkDelete(2);
-};
+    let kayıt = msg.guild.member(msg.mentions.users.first()) || msg.guild.member(args[0]);
+    if (!kayıt) return msg.channel.send({
+        embed: {
+            color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+            description: ("📥 Kendini etiketlemelisin!")
+        }
+    })
 
+    let role = msg.guild.roles.find(r => r.name === "{--•°Üye°•--}");
+    if (!role) {
+        try {
+            role = await msg.guild.Role({
+            });
+
+            msg.guild.channels.forEach(async (channel, id) => {
+                await channel.overwritePermissions(role, {
+                    SEND_MESSAGES: false,
+                    ADD_REACTIONS: false
+                });
+            });
+        } catch (e) {
+            console.log(e.stack);
+        }
+
+    }
+
+    if (kayıt.roles.has(role.id)) return msg.channel.send({
+        embed: {
+            color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+            description: (" 💡 Zaten önceden kayıt olmuşsun! ")
+        }
+    });
+
+    await kayıt.addRole(role);
+    msg.channel.send({
+        embed: {
+            color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+            description: ("🔑 Tebrikler kayıt oldunuz!")
+        }
+    })
+}
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['kayit', 'kaydol'],
-  permLevel: 0
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: 0
 };
 
 exports.help = {
-  name: 'kayıt',
-  description: 'Kayıt olmak için',
-  usage: 'kayıt'
+    name: 'kayıt-ol',
+    description: 'Sunucuya kayıt olursunuz!',
+    usage: 'kayıt-ol'
 };
