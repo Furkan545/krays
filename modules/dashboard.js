3c394e0748add3e625fdf1691387693b4659c9ca
Glitch (osman8x) <none>
2019-05-02 13:53:07 +0000

    ❄️📇 Checkpoint ./komutlar/hastebin.js:3756202/1041 ./komutlar/hatabildir.js:3756202/1604 ./modules/dashboard.js:3756202/36 ./komutlar/namaz-vakti.js:3756202/1827

--- modules/dashboard.js
+++ modules/dashboard.js
@@ -113,7 +113,7 @@ module.exports = (client) => {
   app.get("/autherror", (req, res) => {
     renderTemplate(res, req, "autherror.ejs");
     
-    client.channels.get("572884179063406603").send("Web Panelinde bağlantı hatası oluştu! Kişi giriş yapamıyor tekrar denemeli! Büyük bir sorun değil.")
+    client.channels.get("573505484636553217").send("Web Panelinde bağlantı hatası oluştu! Kişi giriş yapamıyor tekrar denemeli! Büyük bir sorun değil.")
   });
 
   app.get("/logout", function(req, res) {
