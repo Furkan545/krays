c39649f4608541a5ea8ef6914858fbe2341b995d
Glitch (osman8x) <none>
2019-05-04 14:33:05 +0000

    🚤🙇 Checkpoint ./komutlar/yardım1.js:3756202/315

--- komutlar/yardım1.js
+++ komutlar/yardım1.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
 
   let pages = [
-              '**Botu Davet Etmek İçin**\n\n\n' + '``h!davet`` Yazabilirsin. \nSunucunuzda Resimli Giriş Çıkışın Çalışması İçin ``giriş-çıkış`` Adında Bir Metin Kanalı Olmalıdır.',
+              '**<a:emoji_13:574240268434276352>Bot Bilgiler**\n\n\n' + '``h!davet`` Yazabilirsin. \nSunucunuzda Resimli Giriş Çıkışın Çalışması İçin ``giriş-çıkış`` Adında Bir Metin Kanalı Olmalıdır.',
               '**Yenilikler**\n\n\n' + '``-``  h!nsfw-gif = NSFW Özelliklerine ``GIF`` Seçeneği Eklendi. \n``-``  h!desteksunucusu =  Destek Sunucusunun Linkini Atar.',
               '**Eğlence 1**\n\n\n' + '``-``  h!1vs1 @kullanıcı @kullanıcı = Belirlediğin İki Kişiyi Kapıştırır. \n``-``  h!aşkölçer @kullanıcı = Belirlediğin Kullanıcıyla Aşk Durumunu Ölçer. \n ``-``  h!atasözü = Rastgele Bir Atasözü Söyler. \n``-``  h!atatürk = Atatürk GIFI Atar. \n``-``  h!ateş-et @kullanıcı = Belirlediğin Kullanıcıya Ateş Edersin. \n``-``  h!atom-at  = Atom Bombası GIFI Atar. \n``-``  h!balık-tut = Rastgele Balık Tutarsın. \n ``-`` h!bayrak  = TÜRK Bayrağı GIFI Atar. \n``-``  h!cool = Rastgele Cool Resimler Atar. \n``-``  h!csgo (Hesap İsmi) = Belirlediğin CS:GO Hesabının İstatistiklerine Bakarsın. \n``-``  h!çay-doldur = Çay Doldurursun. \n``-``  h!çay-iç = Çay İçersin. \n``-``  h!espri = Rastgele Espri Yapar. \n``-``  h!evlen = Evlenirsin. \n``-``  h!fortnite (Hesap İsmi) = Belirlediğin Fortnite Hesabının İstatistiklerine Bakarsın.',
               '**Eğlence 2**\n\n\n' + '``-``  h!google (Aranacak Kelime) = Yazdığın Kelimeyi Google da Arar. \n``-``  h!hackle @kullanıcı = Belirlediğin Kullanıcıyı Hacklersin. \n ``-``  h!hava-durumu (Şehir) = Belirlediğin Şehrin Hava Durumuna Bakarsın. \n``-``  h!kaçcm = Malafatın Boyunu Ölçer. \n``-``  h!kedi = Rastgele Kedi Resmi Atar. \n``-``  h!kedi-adı  = Rastgele Kedi İsmi Söyler. \n``-``  h!korkut = Bot Seni Korkutur. \n ``-`` h!koş  = Koşarsın. \n``-``  h!köpek = Rastgele Köpek Resimler Atar. \n``-``  h!köpek-adı = Rastgele Köpek İsmi Söyler. \n``-``  h!nahçek = Jahrein in ``Karma İs A Bitch`` GIFINI Atar. \n``-``  h!öp = Öpüşme GIFI Atar. \n``-``  h!piyango = Piyango Çekersiniz. \n``-``  h!playstore (Oyun İsmi) = PlayStore Üzerinden Oyun Ararsınız. \n``-``  h!polis = Polis Çağırırsın.',
@@ -54,7 +54,7 @@ exports.run = (client, message, args) => {
 
 
 exports.conf = {
-enabled: true,
+enabled: false,
 guildOnly: true,
 aliases: ["y"],
 permLevel: 0
