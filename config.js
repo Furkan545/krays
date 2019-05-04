d41c0407990332740d5e70ff9f197150839d3667
Glitch (osman8x) <none>
2019-05-04 16:34:05 +0000

    👴🖖 Checkpoint ./ayarlar.json:3756202/118 ./config.js:3756202/118 ./index.js:3756202/118

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   "ownerID": "495679360271908864", //kendi IDınızı yazınız
   "admins": ["495679360271908864"],
   "support": ["495679360271908864"],
-  "token": "NTc0MjY0OTUyNzgxNzk5NDY1.XM238A.ZLy7tRkCAC3bpEiUBfNNbKnS1tw", //botunuzun tokenini yazınız
+  "token": "NTc0MjY0OTUyNzgxNzk5NDY1.XM27cA.TbcUVXbr5IDkcOnxDYVT9oHT04s", //botunuzun tokenini yazınız
   "dashboard" : {
     "oauthSecret": "vjgjLGg4zZDrYJfQU_9HW44m2zLNI65N", //botunuzun secretini yazınız 
     "callbackURL": "",//site URLnizi yazınız /callback kısmını silmeyiniz!
