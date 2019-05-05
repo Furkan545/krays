f5277a641b1d4e41ab6f942eb337ab752f710304
Glitch (osman8x) <none>
2019-05-05 15:49:04 +0000

    🏚👋 Checkpoint ./komutlar/yetkililer.js:3756202/83

--- komutlar/yetkililer.js
+++ komutlar/yetkililer.js
@@ -5,7 +5,7 @@ exports.run = async (client, message) => {
   var str = ''
 for(var i = 0; i < message.guild.members.size; i++) {
    if(message.guild.members.array()[i].hasPermission("MANAGE_MESSAGES") && message.guild.members.array()[i].presence.status === "dnd" && !message.guild.members.array()[i].user.bot) {
-      str += `${client.emojis.get('574612597391884288')} ${message.guild.members.array()[i].user.tag}\n`
+      str += `${client.emojis.get('574614435872899082')} ${message.guild.members.array()[i].user.tag}\n`
     }
     else if(message.guild.members.array()[i].hasPermission("MANAGE_MESSAGES") && message.guild.members.array()[i].presence.status === "online" && !message.guild.members.array()[i].user.bot){
       str += `${client.emojis.get('574612554689806336')} ${message.guild.members.array()[i].user.tag}\n`
@@ -21,7 +21,7 @@ for(var i = 0; i < message.guild.members.size; i++) {
   var embed = new Discord.RichEmbed()
   .setColor("RANDOM")
   .setAuthor(`${message.guild.name} - Sunucu Yetkilileri`)
-  .setDescription(str.replace(message.guild.owner.user.tag, `${client.emojis.get("516215213960462336")} **${message.guild.owner.user.tag}**`))
+  .setDescription(str.replace(message.guild.owner.user.tag, `${client.emojis.get("574005374269653006")} **${message.guild.owner.user.tag}**`))
   .setThumbnail(message.guild.iconURL)
   .setFooter("NOT: Bu komut sunucudaki \"Mesajları Yönet\" iznine sahip kullanıcıları listeler.")
 message.channel.send(embed)
@@ -29,7 +29,7 @@ message.channel.send(embed)
 };
 
 exports.conf = {
-  enabled: false,
+  enabled: true,
   guildOnly: false,
   aliases: ['yetkili-listesi', 'sunucu-yetkilileri', 'moderatorler', 'modlar', 'moderatörler', 'staffs'],
   permLevel: 0,
