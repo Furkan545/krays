bab4c3275490a4f1fdd779e0df6bd57f69bcdc91
Glitch (osman8x) <none>
2019-05-06 01:16:04 +0000

    🏘😻 Checkpoint ./komutlar/yardım1.js:3756202/83

--- komutlar/yardım1.js
+++ komutlar/yardım1.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot, message, args) => {
     .setColor("RANDOM")
     .setTitle(`<a:emoji_12:574240268434276352> <a:emoji_3:574005374269653006> KRAY YARDIM <a:emoji_3:574005374269653006> <a:emoji_13:574240299820384257>`)
     .setThumbnail('http://www.gifgratis.net/gifs_animes/poules/38.gif')
-    .addField("▬ Bir komut hakkında bilgi almak için ▬", `<a:emoji_3:574005374269653006> kp!bilgi <komut adı> \n<a:emoji_3:574005374269653006> kp!roller ▬ kp!emojiler`)
-    .addField(`▬ Aktif Komutlar ▬`, `<a:4911_dynopspin:573890327535157248> kp!kulanıcı 》 Kullanıcı komutlarını listeler. \n<a:4911_dynopspin:573890327535157248> kp!eğlence 》 Eğlence komutlarını listeler. \n<a:4911_dynopspin:573890327535157248> kp!`)
+    .addField("▬ Bir komut hakkında bilgi almak için ▬", `<a:emoji_3:574005374269653006> k+bilgi <komut adı> \n<a:emoji_3:574005374269653006> k+roller ▬ k+emojiler`)
+    .addField(`▬ Aktif Komutlar ▬`, `<a:4911_dynopspin:573890327535157248> k+kulanıcı 》 Kullanıcı komutlarını listeler. \n<a:4911_dynopspin:573890327535157248> k+eğlence 》 Eğlence komutlarını listeler. \n<a:4911_dynopspin:573890327535157248> k+sunucu 》 Sunucu komutlarını listeler.`)
     message.channel.send(serverembed);
 }
 
