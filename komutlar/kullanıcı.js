b19609e131a30a65df62d285c116b7bfeaa9fe56
Glitch (kray1) <none>
2019-05-07 02:39:06 +0000

    🎃📖 Checkpoint ./komutlar/kullanıcı.js:3756202/134

--- komutlar/kullanıcı.js
+++ komutlar/kullanıcı.js
@@ -8,7 +8,9 @@ exports.run = (client, message, args) => {
     .setColor("RANDOM")
     .setAuthor(`${client.user.username} Kullanıcı Komutları`)
     .addField('k+afk', 'Afk Olursunuz.')//ne kadar yetkili komutunuz varsa o kadar .addField('prefix+komut', 'açıklama/kullanım amacı') koyun
-    .addField ('k+', '')
+    .addField ('k+hesapla', 'Belirtilen İşlemi Yapar.')
+    .addField ('k+söz', 'Rastgele güzel sözler atar.')
+    .addFiedl ('k+atatürk', '')
     .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/BAĞLANTI) **|** [Bota Oy Ver (Vote)](https://discordbots.org/bot/${botid}/vote) **|** [Web Sitesi]()`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
     message.channel.sendEmbed(embed);
 
