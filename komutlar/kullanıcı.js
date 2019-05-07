ff1eacbf78f0aa5dabd97cf14a5e36ee83a4be03
Glitch (kray1) <none>
2019-05-07 03:12:06 +0000

    🚣🏑 Checkpoint ./komutlar/kullanıcı.js:3756202/135 ./komutlar/müzik.js:3756202/1375 ./komutlar/eğlence.js:3756202/1048

--- komutlar/kullanıcı.js
+++ komutlar/kullanıcı.js
@@ -7,13 +7,16 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setColor("RANDOM")
     .setAuthor(`${client.user.username} Kullanıcı Komutları`)
-    .addField('prefix+komut', 'açıklama/kullanım amacı')//ne kadar yetkili komutunuz varsa o kadar .addField('prefix+komut', 'açıklama/kullanım amacı') koyun
+    .addField('prefix+komut', 'Açıklama')//ne kadar yetkili komutunuz varsa o kadar .addField('prefix+komut', 'açıklama/kullanım amacı') koyun
     .addField ('', '')
     .addField ('', '')
     .addField ('', '')
     .addField ('', '')
     .addField ('', '')
-    .addFie
+    .addField ('', '')
+    .addField ('', '')
+    .addField ('', '')
+    .addField ('', '')
     .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=574264952781799465&scope=bot&permissions=0) **|** [Destek Sunucusu](https://discord.gg/ss3N4hM) **|** [Bota Oy Ver (Vote)](https://discordbots.org/bot/${botid}/vote) **|** [Web Sitesi]()`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
     message.channel.sendEmbed(embed);
 
