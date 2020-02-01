04f85590b8fb1271f3b34648dea0194bd6c87166
Furkan <48797120+Furkan545@users.noreply.github.com>
2020-02-01 21:55:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const config = {
   "ownerID": "495679360271908864", //kendi IDınızı yazınız
   "admins": ["495679360271908864"],
   "support": ["495679360271908864"],
-  "token": "NTc0MjY0OTUyNzgxNzk5NDY1.XM27cA.TbcUVXbr5IDkcOnxDYVT9oHT04s", //botunuzun tokenini yazınız
+  "token": "NTc0MjY0OTUyNzgxNzk5NDY1YVT9oHT04s", //botunuzun tokenini yazınız
   "dashboard" : {
-    "oauthSecret": "vjgjLGg4zZDrYJfQU_9HW44m2zLNI65N", //botunuzun secretini yazınız 
+    "oauthSecret": "vjgj5555Gg4zZDrYJfQU_9m42zLNI65N", //botunuzun secretini yazınız 
     "callbackURL": "",//site URLnizi yazınız /callback kısmını silmeyiniz!
     "sessionSecret": "9999999999999999999999999", //kalsın
     "domain": "" //site URLnizi yazınız!
